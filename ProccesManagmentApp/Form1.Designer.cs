a3e17be7da0f74106cb5592a5907a30430804387
Omer Hanan <omer.hanan@gmail.com>
2022-04-17 18:39:47 +0300

    bug fix

--- ProccesManagmentApp/Form1.Designer.cs
+++ ProccesManagmentApp/Form1.Designer.cs
@@ -94,7 +94,7 @@
             this.Controls.Add(this.button1);
             this.Controls.Add(this.listView1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Name = "TaskManagmentApp";
+            //this.Name = "TaskManagmentApp";
             this.Text = "Procces Managment App - OS course";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
